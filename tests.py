fffe9b111b8518072a7e340890d5a47d31a8c42d
Valerii Lashmanov <vflashm@gmail.com>
2017-09-18 21:53:36 -0400

    tests actually work

--- tests.py
+++ tests.py
@@ -25,23 +25,21 @@ def create(root, desc):
             os.mkdir(path)
         else:
             with open(path, 'w') as f:
-                f.write(content or name)
+                f.write(content or os.path.basename(name))
 
 def walk(root_path):
     for root, dirs, files in os.walk(root_path):
         for name in files:
             path = os.path.join(root, name)
-            rel_path = os.path.relpath(root_path, path)
+            rel_path = os.path.relpath(path, root_path)
             with open(path) as f:
                 content = f.read()
-            yield rel_path, content
+            yield rel_path.replace('\\', '/'), content
         for name in dirs:
             path = os.path.join(root, name)
-            rel_path = os.path.relpath(root_path, path)
-            yield rel_path + '/', None
-
-def check(root, desc):
-    return dict(parse(desc)) == dict(walk(root))
+            if not os.listdir(path):
+                rel_path = os.path.relpath(path, root_path)
+                yield rel_path.replace('\\', '/') + '/', None
     
 
 class RenameTest(unittest.TestCase):
@@ -52,6 +50,14 @@ class RenameTest(unittest.TestCase):
     def check(self, desc):
         return check(self.root, desc)
 
+    def check(self, desc):
+        parsed_desc = dict(parse(desc))
+        for key in parsed_desc:
+            if not key.endswith('/'):
+                if parsed_desc[key] is None:
+                    parsed_desc[key] = os.path.basename(key)
+        self.assertEqual(parsed_desc, dict(walk(self.root)))
+
     def setUp(self):
         self.root_obj = tempfile.TemporaryDirectory()
         self.root = self.root_obj.name
@@ -61,13 +67,27 @@ class RenameTest(unittest.TestCase):
         self.root_obj = None
         self.root = None
     
-    def test_simple(self):
+    def test_files(self):
         self.create('''
             a
             b
             c
             d
+        ''')
+        rerename.rename(self.root, parse('''
+            a = 1
+            b = 2
+            c = 3
+        '''), False)
+        self.check('''
+            1 = a
+            2 = b
+            3 = c
+            d
+        ''')
 
+    def test_dirs(self):
+        src = '''
             e/
             f/
 
@@ -76,20 +96,15 @@ class RenameTest(unittest.TestCase):
 
             h/h1
             h/h2
-        ''')
+        '''
+        self.create(src)
+
+        # without tail slash
         rerename.rename(self.root, parse('''
-            a = 1
-            b = 2
-            c = 3
             e = 4
-            g/ = 5
+            g = 5
         '''), False)
         self.check('''
-            1 = a
-            2 = b
-            3 = c
-            d
-
             4/
             f/
 
@@ -99,3 +114,40 @@ class RenameTest(unittest.TestCase):
             h/h1
             h/h2
         ''')
+
+        # with tail slash
+        rerename.rename(self.root, parse('''
+            4/ = e
+            5/ = g
+        '''), False)
+        self.check(src)
+
+    # def test_overwrite(self):
+    #     self.create('''
+    #         a
+    #         b
+
+    #         c/
+    #         d/
+
+    #         e/e1
+    #         e/e2
+
+    #         f/f1
+    #         f/f2
+    #     ''')
+    #     rerename.rename(self.root, parse('''
+    #         a = b
+    #         c = d
+    #         e = f
+    #     '''), True)
+    #     self.check('''
+    #         b = a
+    #         d = c
+
+    #         f/e1
+    #         f/e2
+
+    #         f/h1
+    #         f/h2
+    #     ''')
